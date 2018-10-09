4f2974a09efb76118b52b289a104065ab2a183f2
chrisgsamp <chrisgsamp@gmail.com>
2018-10-09 10:08:30 -0400

    Complete second exercise

---
+++ src/main/java/io.pivotal.pal.tracker/EnvController.java
@@ -0,0 +1,32 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class EnvController {
+
+    private HashMap<String, String> env = new HashMap<String, String>();
+
+    @Autowired
+    public EnvController(
+            @Value("${PORT:NOT SET}") String port,
+            @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+            @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+            @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddr
+    ) {
+        env.put("PORT",port);
+        env.put("MEMORY_LIMIT",memoryLimit);
+        env.put("CF_INSTANCE_INDEX",cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR",cfInstanceAddr);
+    }
+
+    @GetMapping("/env")
+    public HashMap<String, String> getEnv(){
+        return env;
+    }
+}
